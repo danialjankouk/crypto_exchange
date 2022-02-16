0d5a7c6986dc8870e8512ef8bce6bd0d61a0fb3b
danialjankouk <dani.jankook@gmail.com>
2022-02-16 23:06:09 +0330

    styles done

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,19 +1,19 @@
-import React from 'react';
-
-const Coin = ({coinData}) => {
-    return (
-        <>
-        <div>
-          <span>{coinData.market_cap_rank}</span>
-          <img src={coinData.image} alt="coinImage" />
-          <span>{coinData.name}</span>
-          <span>{coinData.symbol.toUpperCase()}</span>
-          <span>{coinData.current_price.toLocaleString()}</span>
-          <span>{coinData.price_change_percentage_24h}</span>
-          <span>{coinData.market_cap.toLocaleString()}</span>
-        </div>
-      </>
-    );
+import React from "react";
+import styles from "./Coin.module.css"
+const Coin = ({ coinData }) => {
+  return (
+    <>
+      <div className={styles.container}>
+        <span className={styles.rank}>{coinData.market_cap_rank}</span>
+        <img src={coinData.image} alt="coinImage"className={styles.logo} />
+        <span>{coinData.name}</span>
+        <span>{coinData.symbol.toUpperCase()}</span>
+        <span>{coinData.current_price.toLocaleString()}</span>
+        <span>{coinData.price_change_percentage_24h}</span>
+        <span>{coinData.market_cap.toLocaleString()}</span>
+      </div>
+    </>
+  );
 };
 
 export default Coin;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,19 +2,20 @@ import React,{useState,useEffect} from 'react';
 import styles from "./Landing.module.css"
 import Loader from './Loader';
 import Coin from './Coin';
-import TitleBar from './TitleBar';
 //api
 import { getCoin } from '../service/api';
 
 const Landing = () => {
     const [data, setData] = useState([])
     const [change, setChange] = useState("")
-
+    //changeHandler input
     const changeHandler = (event) => {
         setChange(event.target.value);
     }
+
+    //search engin
     const searchCoins = data.filter((coin) => coin.name.toLowerCase().includes(change.toLowerCase()))
-    
+    //get api
     useEffect(() => {
         const fetchApi = async() => {
             const data =await getCoin()
@@ -26,8 +27,7 @@ const Landing = () => {
 
     return (
       <>
-        <input placeholder="Search" type="text" onChange={changeHandler} />
-        <TitleBar />
+        <input placeholder="Search" type="text" onChange={changeHandler} className={styles.search}/>
         <div className={styles.container}>
           {data.length ? (
             searchCoins.map((item) => <Coin key={item.id} coinData={item} />)
